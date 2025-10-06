c00defe2307ace28a0034f6bad5d4c1827201cd7
lalitprakash987 <lalitprakash987@gmail.com>
2025-10-06 18:11:57 +0530

    latest push

--- src/test/java/com/autoseat/base/BaseClass.java
+++ src/test/java/com/autoseat/base/BaseClass.java
@@ -2,8 +2,12 @@ package com.autoseat.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -11,11 +15,19 @@ public class BaseClass {
 
 	public WebDriver driver;
 
+	@Parameters("browser")
 	@BeforeMethod
-	public void setUp() {
-		// WebDriverManager automatically driver download and setup karega
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+	public void setUp(@Optional("chrome") String browser) {
+		if (browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (browser.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
 		driver.manage().window().maximize();
 		driver.get("https://seatpal.co.uk/");
 	}
